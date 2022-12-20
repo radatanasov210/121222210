f4977d5def2e89b735d4eaa0cea4479b731633b0
radatanasov210 <115942521+radatanasov210@users.noreply.github.com>
2022-12-20 19:00:32 +0200

    Create user.py

---
+++ bank/user.py
@@ -0,0 +1,19 @@
+from errors import UserAlreadyOwnsAccount,AccountNotFound
+from account import Account
+
+class User:
+    def __init__(self, names, egn) -> None:
+        self.names = names
+        self.egn = egn
+        self.accounts = []
+
+    def get_print(self):
+        return f"User({self.names}, {self.egn}, accounts: [{len(self.accounts)}])"
+
+    def add_account(self, account):
+        if account in self.accounts:
+            raise UserAlreadyOwnsAccount("Error: This account already belongs to this user")
+            
+        self.accounts.append(account)
+    def find_by_Iban(self,IBAN) -> Account:
+        pass
